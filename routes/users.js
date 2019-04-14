10afd8b0292d5f9554d05498318fdfbfe2eed007
Nikko Enggaliano <37658579+nikkoenggaliano@users.noreply.github.com>
2019-04-14 18:58:36 +0700

    Fixing registrator issue

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const crypto = require('crypto');
 const config = require('../config.js');
 const bcrypt = require('bcrypt');
+const valid  = require('email-validator');
 
 
 
@@ -12,21 +12,50 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/register', function(req,res,next){
-	let email = req.body.email;
+	let email = req.body.email.toLowerCase();;
 	let user  = req.body.username;
 	let pass  = req.body.pass1;
+	let pass2 = req.body.pass2;
 	let fpass  = bcrypt.hashSync(pass,10); //hash the pass
 	let stat  = 1;
-	//{ email: 'asd', username: 'asd', pass1: 'asd', pass2: 'asd' }
+
+	if(user.length < 5 || pass.length < 5){
+		req.flash('type', 'error');
+		req.flash('message', 'Username atau Password minimal 5 karakter.');
+		res.redirect('/auth');
+		return false;
+	}
+
+	if(!valid.validate(email)){
+		req.flash('type', 'error');
+		req.flash('message', 'Tolong masukan email yang valid!');
+		res.redirect('/auth');
+		return false;
+	}
+
+	if(pass != pass2){
+		req.flash('type', 'error');
+		req.flash('message', 'Password yang anda masukan tidak sama!');
+		res.redirect('/auth');
+		return false;
+	}
 	let query = "INSERT INTO `user` (`id`,`email`,`username`,`password`) VALUES (NULL,?,?,?)";
 	let data  = [email,user,fpass];
 	db.query(query, data, (err,result,field) =>{
 		console.log(err);
 		console.log(result);
+		
+		if(err.code == 'ER_DUP_ENTRY'){
+			req.flash('type', 'error');
+			req.flash('message', 'Maaf email/username telah terpakai!');
+			res.redirect('/auth');
+			return false;
+		}
+
 		if(!err){
 			if(result.affectedRows == 1){
-				req.flash('info', 'success');
-				req.flash('status', 'Selamat Registrasi Berhasil');
+				req.flash('type', 'success');
+				req.flash('message', 'Selamat Registrasi Berhasil');
 				res.redirect('/auth');
 
 			}
